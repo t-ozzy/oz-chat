e012836a232de02f8a1a7d5c7f724f061a8e51b2
rikeda-cloud <longsichitian648@gmail.com>
2025-07-20 23:01:56 +0900

    [fix] Hydration error

--- apps/fe/src/app/layout.tsx
+++ apps/fe/src/app/layout.tsx
@@ -24,10 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <GlobalThemeProvider>{children}</GlobalThemeProvider>
       </body>
     </html>
--- apps/fe/src/components/ui/GlobalThemeProvider.tsx
+++ apps/fe/src/components/ui/GlobalThemeProvider.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { ChakraProvider } from "@chakra-ui/react"
-import { CacheProvider } from "@chakra-ui/next-js";
-import { system } from "./theme"
+import { ChakraProvider } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import { system } from "./theme";
 
-export function GlobalThemeProvider(props: { children: React.ReactNode }) {
+export function GlobalThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       <ChakraProvider value={system}>
-        {props.children}
+        {children}
       </ChakraProvider>
-    </CacheProvider>
-  )
+    </ThemeProvider>
+  );
 }
