59cd07ed6fbec12872a009891f51d07e40498f59
tachu <roatyungtatyu@gmail.com>
2025-07-24 22:44:45 +0900

    modify: ui

--- apps/fe/src/app/profile/page.tsx
+++ apps/fe/src/app/profile/page.tsx
@@ -14,14 +14,10 @@ export default function Profile() {
   return (
     <Box borderColor="border" backgroundColor="background.default" p={6}>
       <Grid>
-        <GridItem border="2px solid" borderColor="border" pt={5}>
-          <Grid templateColumns="min-content min-content min-content" gap={100}>
+        <GridItem border="2px solid" borderColor="border" pt={5} pl={3}>
+          <Grid templateColumns="min-content min-content min-content">
             {/* 左セクション */}
-            <GridItem
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-            >
+            <GridItem display="flex" justifyContent="flex-end">
               <Avatar.Root size="2xl">
                 <Avatar.Fallback name="profile picture" />
                 <Avatar.Image src="https://ui-avatars.com/api/?name=oz+chat" />
@@ -29,7 +25,7 @@ export default function Profile() {
             </GridItem>
 
             {/* 中央セクションを縦3分割 */}
-            <GridItem minW="200px">
+            <GridItem minW="200px" pl={2}>
               <Grid templateRows="repeat(4, auto)">
                 <GridItem>
                   <Text fontSize="l" fontWeight="medium">
