406b773287fae9cdb36f6f79c9e3f2691c86c581
tachu <roatyungtatyu@gmail.com>
2025-07-24 22:54:43 +0900

    fix: card ui

--- apps/fe/src/app/profile/page.tsx
+++ apps/fe/src/app/profile/page.tsx
@@ -9,54 +9,17 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
+import UserInfo from "./components/UserInfo";
 
 export default function Profile() {
   return (
-    <Box borderColor="border" backgroundColor="background.default" p={6}>
-      <Grid>
-        <GridItem border="2px solid" borderColor="border" pt={5} pl={3}>
-          <Grid templateColumns="min-content min-content min-content">
-            {/* 左セクション */}
-            <GridItem display="flex" justifyContent="flex-end">
-              <Avatar.Root size="2xl">
-                <Avatar.Fallback name="profile picture" />
-                <Avatar.Image src="https://ui-avatars.com/api/?name=oz+chat" />
-              </Avatar.Root>
-            </GridItem>
-
-            {/* 中央セクションを縦3分割 */}
-            <GridItem minW="200px" pl={2}>
-              <Grid templateRows="repeat(4, auto)">
-                <GridItem>
-                  <Text fontSize="l" fontWeight="medium">
-                    oz
-                  </Text>
-                </GridItem>
-                <GridItem mb={4}>
-                  <Text color="fontColor.lightGray">@oz-chat</Text>
-                </GridItem>
-                <GridItem mb={4}>
-                  <Box>description</Box>
-                </GridItem>
-                <GridItem mb={2}>
-                  <HStack spaceX="l">
-                    <Text fontSize="s">0フォロー中</Text>
-                    <Text fontSize="s">0フォロワー</Text>
-                  </HStack>
-                </GridItem>
-              </Grid>
-            </GridItem>
-
-            {/* 右セクション */}
-            <GridItem>
-              <Button size="lg">プロフィール編集</Button>
-            </GridItem>
-          </Grid>
-        </GridItem>
-        <GridItem>
-          <Text pl={4}>ツイート</Text>
-        </GridItem>
-      </Grid>
+    <Box
+      borderColor="border"
+      backgroundColor="background.default"
+      width="fit-content"
+      p={6}
+    >
+      <UserInfo />
     </Box>
   );
 }
