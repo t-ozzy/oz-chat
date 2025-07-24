5264f501e332af9f4ad31b676cd1adac78dedc3b
tachu <roatyungtatyu@gmail.com>
2025-07-24 22:34:12 +0900

    fix: ui

--- apps/fe/src/app/profile/page.tsx
+++ apps/fe/src/app/profile/page.tsx
@@ -12,53 +12,55 @@ import {
 
 export default function Profile() {
   return (
-    <Grid>
-      <GridItem border="2px solid" borderColor="border">
-        <Grid templateColumns="min-content min-content min-content" gap={100}>
-          {/* 左セクション */}
-          <GridItem>
-            <Center>
+    <Box borderColor="border" backgroundColor="background.default" p={6}>
+      <Grid>
+        <GridItem border="2px solid" borderColor="border" pt={5}>
+          <Grid templateColumns="min-content min-content min-content" gap={100}>
+            {/* 左セクション */}
+            <GridItem
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
               <Avatar.Root size="2xl">
                 <Avatar.Fallback name="profile picture" />
                 <Avatar.Image src="https://ui-avatars.com/api/?name=oz+chat" />
               </Avatar.Root>
-            </Center>
-          </GridItem>
+            </GridItem>
 
-          {/* 中央セクションを縦3分割 */}
-          <GridItem minW="200px">
-            <Grid templateRows="repeat(4, auto)">
-              <GridItem>
-                <Text fontSize="l" fontWeight="medium">
-                  oz
-                </Text>
-              </GridItem>
-              <GridItem mb={4}>
-                <Text color="fontColor.lightGray">@oz-chat</Text>
-              </GridItem>
-              <GridItem mb={4}>
-                <Box>description</Box>
-              </GridItem>
-              <GridItem mb={2}>
-                <HStack spaceX="l">
-                  <Text fontSize="s">0フォロー中</Text>
-                  <Text fontSize="s">0フォロワー</Text>
-                </HStack>
-              </GridItem>
-            </Grid>
-          </GridItem>
+            {/* 中央セクションを縦3分割 */}
+            <GridItem minW="200px">
+              <Grid templateRows="repeat(4, auto)">
+                <GridItem>
+                  <Text fontSize="l" fontWeight="medium">
+                    oz
+                  </Text>
+                </GridItem>
+                <GridItem mb={4}>
+                  <Text color="fontColor.lightGray">@oz-chat</Text>
+                </GridItem>
+                <GridItem mb={4}>
+                  <Box>description</Box>
+                </GridItem>
+                <GridItem mb={2}>
+                  <HStack spaceX="l">
+                    <Text fontSize="s">0フォロー中</Text>
+                    <Text fontSize="s">0フォロワー</Text>
+                  </HStack>
+                </GridItem>
+              </Grid>
+            </GridItem>
 
-          {/* 右セクション */}
-          <GridItem>
-            <Button size="lg">プロフィール編集</Button>
-          </GridItem>
-        </Grid>
-      </GridItem>
-      <GridItem>
-        <Center>
-          <Text>ツイート</Text>
-        </Center>
-      </GridItem>
-    </Grid>
+            {/* 右セクション */}
+            <GridItem>
+              <Button size="lg">プロフィール編集</Button>
+            </GridItem>
+          </Grid>
+        </GridItem>
+        <GridItem>
+          <Text pl={4}>ツイート</Text>
+        </GridItem>
+      </Grid>
+    </Box>
   );
 }
