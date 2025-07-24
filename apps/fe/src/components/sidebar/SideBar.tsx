62bce3be3585e3865d9f0bd0273be54a35abf446
rikeda-cloud <longsichitian648@gmail.com>
2025-07-24 14:46:22 +0900

    [refactor] create sideBarNavItems.ts

--- apps/fe/src/components/sidebar/SideBar.tsx
+++ apps/fe/src/components/sidebar/SideBar.tsx
@@ -1,12 +1,6 @@
 import { Box, VStack } from "@chakra-ui/react";
-import { FaHome, FaPencilAlt, FaUser } from "react-icons/fa";
 import SideBarContent from "./SideBarContent";
-
-const menuItems = [
-  { href: "/articles", label: "ホーム", icon: FaHome },
-  { href: "/profile", label: "プロフィール", icon: FaUser },
-  { href: "/articles/create", label: "記事投稿", icon: FaPencilAlt },
-];
+import sideBarNavItems from "./sideBarNavItems";
 
 export default function SideBar() {
   return (
@@ -20,7 +14,7 @@ export default function SideBar() {
       borderColor="borderColor.main"
     >
       <VStack as="nav" spacing={4} align="stretch" pt={8}>
-        {menuItems.map((item) => (
+        {sideBarNavItems.map((item) => (
           <SideBarContent
             href={item.href}
             label={item.label}
