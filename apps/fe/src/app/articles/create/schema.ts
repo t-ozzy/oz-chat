4d10b119c937a13b7f9fa6b7d34d228650ac9e78
rikeda-cloud <longsichitian648@gmail.com>
2025-07-24 17:38:23 +0900

    [refactor] 280 -> ARTICLE_MAX_LENGTH

--- apps/fe/src/app/articles/create/schema.ts
+++ apps/fe/src/app/articles/create/schema.ts
@@ -6,7 +6,10 @@ export const schema = yup.object({
   content: yup
     .string()
     .required("投稿内容は必須です")
-    .max(280, `投稿内容は${ARTICLE_MAX_LENGTH}文字以内で入力してください`),
+    .max(
+      ARTICLE_MAX_LENGTH,
+      `投稿内容は${ARTICLE_MAX_LENGTH}文字以内で入力してください`,
+    ),
 });
 
 export type ArticleValues = yup.InferType<typeof schema>;
