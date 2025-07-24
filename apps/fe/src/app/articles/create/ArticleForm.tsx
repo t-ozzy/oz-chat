daf1e643ec5f7f218f4a299e8ee98349b75c1d1e
rikeda-cloud <longsichitian648@gmail.com>
2025-07-24 16:51:45 +0900

    [add] Icon to ArticleForm

--- apps/fe/src/app/articles/create/ArticleForm.tsx
+++ apps/fe/src/app/articles/create/ArticleForm.tsx
@@ -2,10 +2,13 @@ import {
   Button,
   Center,
   Field,
+  HStack,
+  Icon,
   Text,
   Textarea,
   VStack,
 } from "@chakra-ui/react";
+import { FaUser } from "react-icons/fa";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { type ArticleValues, schema } from "./schema";
@@ -24,28 +27,30 @@ export default function ArticleForm() {
 
   return (
     <Center>
-      {/* Iconを配置 */}
-      <form onSubmit={handleSubmit(onSubmit, onError)} noValidate>
-        <VStack m="xl">
-          <Field.Root m="xl">
-            <Textarea
-              placeholder="今何してる？"
-              size="xl"
-              {...register("content")}
-            />
-            {errors["content"] && (
-              <Text color="fontColor.error" textStyle="xs">
-                {errors["content"]?.message}
-              </Text>
-            )}
-          </Field.Root>
+      <HStack align="start" p="xl">
+        <Icon as={FaUser} size="2xl" m="xl" />
+        <form onSubmit={handleSubmit(onSubmit, onError)} noValidate>
+          <VStack m="xl">
+            <Field.Root m="xl">
+              <Textarea
+                placeholder="今何してる？"
+                size="xl"
+                {...register("content")}
+              />
+              {errors["content"] && (
+                <Text color="fontColor.error" textStyle="xs">
+                  {errors["content"]?.message}
+                </Text>
+              )}
+            </Field.Root>
 
-          <Button type="submit" w="l" borderRadius="m" m="xl">
-            投稿
-          </Button>
-        </VStack>
+            <Button type="submit" w="l" borderRadius="m" m="xl">
+              投稿
+            </Button>
+          </VStack>
 
-      </form>
+        </form>
+      </HStack>
     </Center>
   )
 }
