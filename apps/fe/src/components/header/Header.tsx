f94a5de0fa16702f3b4ee9c0f0d3c393e23f8aa6
tozeki <toshihiro.722105@gmail.com>
2025-07-24 20:50:55 +0900

    [refactor] run format

--- apps/fe/src/components/header/Header.tsx
+++ apps/fe/src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Link } from "@chakra-ui/react";
+import { Box, Link, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { usePathname } from "next/navigation";
 import { headerPathList } from "./headerPathList";
