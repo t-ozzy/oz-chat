632262e335aa96f304d11c9ca380d5bbfda3caf3
rikeda-cloud <longsichitian648@gmail.com>
2025-07-25 18:07:13 +0900

    [update] /postsページで投稿したPOSTも閲覧可能

--- apps/fe/src/app/posts/page.tsx
+++ apps/fe/src/app/posts/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { Center } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 import Posts from "@/components/posts/Posts";
+import type { RootState } from "@/store/store";
 import { posts } from "./const";
 
 export default function PostsView() {
+  const postState = useSelector((state: RootState) => state.post);
+  const allPosts = [...postState.posts, ...posts];
+
   return (
     <Center minH="100vh">
-      <Posts posts={posts} />
+      <Posts posts={allPosts} />
     </Center>
   );
 }
