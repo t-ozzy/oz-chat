1e94ed9b6af9a72bc103b44ec295a58a373f066f
rikeda-cloud <longsichitian648@gmail.com>
2025-07-25 11:23:32 +0900

    [add] ReduxProvider base

---
+++ apps/fe/src/store/store.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import counterReducer from "./features/counter/counterSlice";
+
+const persistConfig = {
+  key: "root",
+  storage, // storageに永続化
+};
+
+const rootReducer = combineReducers({ // 新規作成したReducerはここに追加していく
+  counter: counterReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
