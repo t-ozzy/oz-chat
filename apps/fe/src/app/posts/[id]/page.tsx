ba41bbd5bef705b938aa09e703108571befc0815
tozeki <toshihiro.722105@gmail.com>
2025-07-25 17:49:37 +0900

    [fix] PostDetailとPostsViewにHeaderコンポーネントを追加

--- apps/fe/src/app/posts/[id]/page.tsx
+++ apps/fe/src/app/posts/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { Avatar, Box, HStack, Text } from "@chakra-ui/react";
 import { useParams } from "next/navigation";
 import { defaultImgUrl, posts } from "@/app/posts/const";
+import Header from "@/components/header/Header";
 
 export default function PostDetail() {
   const params = useParams();
@@ -12,28 +13,31 @@ export default function PostDetail() {
 
   if (!post) return <div>Not Found</div>;
   return (
-    <Box
-      borderWidth="1px"
-      borderColor="black"
-      py="4"
-      px="5"
-      bg="white"
-      width="100%"
-      height="500px"
-    >
-      <HStack>
-        <Avatar.Root colorPalette="gray">
-          <Avatar.Fallback name="O Z" />
-          <Avatar.Image src={defaultImgUrl} />
-        </Avatar.Root>
-        <Text fontSize="xl" fontWeight="bold">
-          {post.name}
-        </Text>
-        <Text fontSize="s" color="gray.700" alignSelf="flex-end">
-          {post.createdAt}
-        </Text>
-      </HStack>
-      <Text>{post.post}</Text>
-    </Box>
+    <>
+      <Header></Header>
+      <Box
+        borderWidth="1px"
+        borderColor="black"
+        py="4"
+        px="5"
+        bg="white"
+        width="100%"
+        height="500px"
+      >
+        <HStack>
+          <Avatar.Root colorPalette="gray">
+            <Avatar.Fallback name="O Z" />
+            <Avatar.Image src={defaultImgUrl} />
+          </Avatar.Root>
+          <Text fontSize="xl" fontWeight="bold">
+            {post.name}
+          </Text>
+          <Text fontSize="s" color="gray.700" alignSelf="flex-end">
+            {post.createdAt}
+          </Text>
+        </HStack>
+        <Text>{post.post}</Text>
+      </Box>
+    </>
   );
 }
--- apps/fe/src/app/posts/page.tsx
+++ apps/fe/src/app/posts/page.tsx
@@ -2,12 +2,16 @@
 
 import { Center } from "@chakra-ui/react";
 import Posts from "@/components/posts/Posts";
+import Header from "@/components/header/Header";
 import { posts } from "./const";
 
 export default function PostsView() {
   return (
-    <Center minH="100vh">
-      <Posts posts={posts} />
-    </Center>
+    <>
+      <Header></Header>
+      <Center minH="100vh">
+        <Posts posts={posts} />
+      </Center>
+    </>
   );
 }
