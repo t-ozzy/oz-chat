c6b106df3795ad70cd9937401029b28a891941d8
tachu <roatyungtatyu@gmail.com>
2025-07-26 18:02:00 +0900

    add: router at top

--- apps/fe/src/app/page.tsx
+++ apps/fe/src/app/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
+import { RootState } from "@/store/store";
 import { Center, Stack, Text } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  const router = useRouter();
+  const accountState = useSelector((state: RootState) => state.account);
+
+  useEffect(() => {
+    if (accountState.username === "") {
+      router.push("/register");
+    } else {
+      router.push("/posts");
+    }
+  }, [accountState, router]);
+
   return (
     <Center>
       <Stack>
