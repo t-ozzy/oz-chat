0db0b9c0b461875ff8985687526895d8129ab9e1
rikeda-cloud <longsichitian648@gmail.com>
2025-07-28 14:48:40 +0900

    [add] Header, Sidebar ingnore setting by /login

--- apps/fe/src/components/layout/layoutConfig.ts
+++ apps/fe/src/components/layout/layoutConfig.ts
@@ -8,6 +8,10 @@ export const layoutConfig: Record<string, LayoutConfig> = {
     showHeader: false,
     showSidebar: false,
   },
+  "/login": {
+    showHeader: false,
+    showSidebar: false,
+  },
   // デフォルト設定
   default: {
     showHeader: true,
