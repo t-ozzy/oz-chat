2679294b7becc67d68108d2982daf0316f0ee57f
rikeda-cloud <longsichitian648@gmail.com>
2025-07-28 18:14:10 +0900

    [update] Search InputUIのスタイルを調整

--- apps/fe/src/app/search/page.tsx
+++ apps/fe/src/app/search/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 
-import { Text } from "@chakra-ui/react";
+import { Box, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
-export default function Home() {
-  return <Text>abc</Text>;
+export default function SearchPage() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <Box p="xl">
+      <Input
+        placeholder="投稿を検索"
+        value={searchQuery}
+        onChange={handleSearchChange}
+        _focus={{ borderColor: "black" }}
+      />
+    </Box>
+  );
 }
