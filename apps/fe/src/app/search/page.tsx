b784fc2f38895d4e00f6d2acffe189dedb572bb2
rikeda-cloud <longsichitian648@gmail.com>
2025-07-28 18:27:13 +0900

    [update] 投稿を検索する機能を追加

--- apps/fe/src/app/search/page.tsx
+++ apps/fe/src/app/search/page.tsx
@@ -1,24 +1,39 @@
 "use client";
 
-import { Box, Input } from "@chakra-ui/react";
+import { Input, VStack } from "@chakra-ui/react";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import Posts from "@/components/posts/Posts";
+import type { Post } from "@/components/posts/type";
+import type { RootState } from "@/store/store";
 
 export default function SearchPage() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [posts, setPosts] = useState<Post[]>([]);
+  const postState = useSelector((state: RootState) => state.post);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
-    console.log(event.target.value);
+    if (!event.target.value) {
+      setPosts([]);
+      return;
+    }
+    const filterdPosts = postState.posts.filter((post) =>
+      post.post.includes(event.target.value),
+    );
+
+    setPosts(filterdPosts);
   };
 
   return (
-    <Box p="xl">
+    <VStack p="xl">
       <Input
         placeholder="投稿を検索"
         value={searchQuery}
         onChange={handleSearchChange}
         _focus={{ borderColor: "black" }}
       />
-    </Box>
+      <Posts posts={posts}></Posts>
+    </VStack>
   );
 }
